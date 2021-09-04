3fc5359fb92563dc73d8d71a1183793b64110539
VPlay <svitalik2002@gmail.com>
2021-09-04 19:30:06 +0300

    Lab14

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i = 0; i < args.length; i++){
+            System.out.println(args[i] + "\t");
+        }
+    }
+}
